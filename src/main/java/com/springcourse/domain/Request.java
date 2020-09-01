fb2ec6626eb2f268ffa70944d20af2016b97c2d4
eliasjr <a49888ce3c@emailmonkey.club>
2020-09-01 15:30:22 -0400

    config serializable

--- src/main/java/com/springcourse/domain/Request.java
+++ src/main/java/com/springcourse/domain/Request.java
@@ -1,5 +1,6 @@
 package com.springcourse.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -17,8 +18,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import org.hibernate.annotations.ManyToAny;
-
 import com.springcourse.domain.enums.RequestState;
 
 import lombok.AllArgsConstructor;
@@ -31,7 +30,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity(name = "request")
-public class Request {
+public class Request implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
