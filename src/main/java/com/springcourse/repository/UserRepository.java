07f543f3f88e34979d309a1de0a351a1f08ce429
eliasjr <a49888ce3c@emailmonkey.club>
2020-09-22 13:53:00 -0400

    user repository

---
+++ src/main/java/com/springcourse/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.springcourse.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springcourse.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
