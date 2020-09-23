abac36892366f3a8c25101b9226a25cbc4d541e4
eliasjr <a49888ce3c@emailmonkey.club>
2020-09-23 13:23:31 -0400

    teste e ajuste repositorio

--- src/main/java/com/springcourse/repository/UserRepository.java
+++ src/main/java/com/springcourse/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.springcourse.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springcourse.domain.User;
@@ -8,4 +11,7 @@ import com.springcourse.domain.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	@Query("SELECT u FROM user u WHERE u.email = ?1 AND u.password = ?2")
+	public Optional<User> login(String email, String senha);
+	
 }
---
+++ src/test/java/com/springcourse/repository/UserRepositoryTests.java
@@ -0,0 +1,68 @@
+package com.springcourse.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.springcourse.domain.User;
+import com.springcourse.domain.enums.Role;
+
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@SpringBootTest
+public class UserRepositoryTests {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	public void AsaveTest() {
+		User user = new User(null, "Elias", "jr@gmail.com", "123", Role.ADMINISTRATOR, null, null);
+		User createdUser = userRepository.save(user);
+		
+		assertThat(createdUser.getId()).isEqualTo(1L);
+	}
+	
+	@Test
+	public void updateTest() {
+		User user = new User(1L, "Elias Araujo", "jr@gmail.com", "123", Role.ADMINISTRATOR, null, null);
+		User updatedUser = userRepository.save(user);
+		
+		assertThat(updatedUser.getName()).isEqualTo("Elias Araujo");
+	}
+	
+	@Test
+	public void getByIdTest() {
+		Optional<User> result = userRepository.findById(1L);
+		
+		User user = result.get();
+		
+		assertThat(user.getPassword()).isEqualTo("123");
+		
+	}
+	
+	@Test
+	public void listTest() {
+		List<User> users = userRepository.findAll();
+		
+		assertThat(users.size()).isEqualTo(1);
+	}
+	
+	@Test
+	public void loginTest() {
+		Optional<User> result = userRepository.login("jr@gmail.com", "123");
+		
+		User loggedUser = result.get();
+		
+		assertThat(loggedUser.getId()).isEqualTo(1L);
+	}
+}
